08b98366c02812b2139d7dff5837a6e863e19590
masayasviel <33654883+masayasviel@users.noreply.github.com>
2024-12-14 16:04:43 +0900

    :recycle: upper camel

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,40 +1,40 @@
 import { boolean, int, mysqlTable, text, timestamp, uniqueIndex, varchar } from 'drizzle-orm/mysql-core';
 
-export const user = mysqlTable('user', {
+export const User = mysqlTable('user', {
   id: int('id').autoincrement().primaryKey(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   name: varchar('name', { length: 256 }).notNull(),
   code: varchar('code', { length: 256 }).notNull().unique('uniq_code'),
 });
 
-export const tag = mysqlTable('tag', {
+export const Tag = mysqlTable('tag', {
   id: int('id').autoincrement().primaryKey(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   createdBy: int('created_by')
     .notNull()
-    .references(() => user.id),
+    .references(() => User.id),
   name: varchar('name', { length: 256 }).notNull().unique('uniq_name'),
   isOfficial: boolean('is_official').notNull().default(false),
 });
 
-export const userTagRelation = mysqlTable(
+export const UserTagRelation = mysqlTable(
   'user_tag_relation',
   {
     id: int('id').autoincrement().primaryKey(),
     createdAt: timestamp('created_at').notNull().defaultNow(),
     userId: int('user_id')
       .notNull()
-      .references(() => user.id),
+      .references(() => User.id),
     tagId: int('tag_id')
       .notNull()
-      .references(() => tag.id),
+      .references(() => Tag.id),
   },
   (table) => ({
     uniqUserAndTag: uniqueIndex('user_and_tag_uniq').on(table.userId, table.tagId),
   }),
 );
 
-export const article = mysqlTable(
+export const Article = mysqlTable(
   'article',
   {
     id: int('id').autoincrement().primaryKey(),
@@ -47,24 +47,24 @@ export const article = mysqlTable(
     articlePath: text('article_path').notNull(),
     userId: int('user_id')
       .notNull()
-      .references(() => user.id),
+      .references(() => User.id),
   },
   (table) => ({
     uniqUserAndArticleTitle: uniqueIndex('article_title_and_user_uniq').on(table.userId, table.title),
   }),
 );
 
-export const articleTagRelation = mysqlTable(
+export const ArticleTagRelation = mysqlTable(
   'article_tag_relation',
   {
     id: int('id').autoincrement().primaryKey(),
     createdAt: timestamp('created_at').notNull().defaultNow(),
     articleId: int('article_id')
       .notNull()
-      .references(() => article.id),
+      .references(() => Article.id),
     tagId: int('tag_id')
       .notNull()
-      .references(() => tag.id),
+      .references(() => Tag.id),
   },
   (table) => ({
     uniqArticleAndTag: uniqueIndex('article_and_tag_uniq').on(table.articleId, table.tagId),
