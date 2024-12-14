52927384d353839a595940b0871d3a703aab99c1
masayasviel <33654883+masayasviel@users.noreply.github.com>
2024-12-14 16:01:48 +0900

    :art:

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import {drizzle} from 'drizzle-orm/mysql2';
+import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 
 const poolConnection = mysql.createPool({
