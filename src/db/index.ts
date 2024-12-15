f1917e83f1f382f6eb8aeb84911f78d1c9b07d5b
masayasviel <33654883+masayasviel@users.noreply.github.com>
2024-12-15 14:59:19 +0900

    :construction: トポロジカルソートの導入

--- src/db/index.ts
+++ src/db/index.ts
@@ -12,6 +12,39 @@ const QUERY = `SELECT
 FROM
   information_schema.KEY_COLUMN_USAGE`;
 
+function topologicalSort(dependencyMap: Map<string, Set<string>>): string[] {
+  const sorted: string[] = [];
+  const visited: Set<string> = new Set();
+  const tempMarked: Set<string> = new Set();
+
+  const visit = (node: string): void => {
+    if (tempMarked.has(node)) {
+      throw new Error(`${node}で閉路を検出`);
+    }
+
+    if (!visited.has(node)) {
+      tempMarked.add(node);
+
+      const dependencies = dependencyMap.get(node) || new Set();
+      for (const dep of dependencies) {
+        visit(dep);
+      }
+
+      tempMarked.delete(node);
+      visited.add(node);
+      sorted.push(node);
+    }
+  };
+
+  for (const node of dependencyMap.keys()) {
+    if (!visited.has(node)) {
+      visit(node);
+    }
+  }
+
+  return sorted;
+}
+
 async function main(): Promise<void> {
   const poolConnection = mysql.createPool({
     host: process.env.MYSQL_HOST,
@@ -23,7 +56,7 @@ async function main(): Promise<void> {
 
   const db = drizzle({ client: poolConnection });
 
-  const tables_relation_map = new Map<string, Set<string>>(
+  const dependencyMap = new Map<string, Set<string>>(
     R.entries(Schema)
       .filter(([_, v]) => v instanceof Table)
       .map(([_, v]) => [getTableName(v), new Set<string>()]),
@@ -37,16 +70,18 @@ async function main(): Promise<void> {
 
   // biome-ignore lint/style/useConst: <explanation>
   for (let r of relations) {
-    const s = tables_relation_map.get(r.table_name);
+    const s = dependencyMap.get(r.table_name);
     if (s) {
       s.add(r.reference_table_name);
-      tables_relation_map.set(r.table_name, s);
+      dependencyMap.set(r.table_name, s);
     } else {
       throw Error('schemeに定義していないテーブルがある');
     }
   }
 
-  console.log(tables_relation_map);
+  const sorted = topologicalSort(dependencyMap);
+
+  console.log(sorted);
 
   await poolConnection.end();
 }
